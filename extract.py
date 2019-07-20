a4392b18fe6a09d76a99fae86e42400f5d37807d
Ariel Schwartz <als364@cornell.edu>
2019-07-19 22:47:25 -0400

    maybe add all the rest of the changes too

--- extract.py
+++ extract.py
@@ -4,8 +4,18 @@ import re
 
 import teams
 
-with open("example.html") as file:
-  soup = BeautifulSoup(file, "html.parser")
+###############################################################################
+# get_penalties_from_game
+#
+# Inputs:
+#   html: A string of raw html, scraped from a hockey-reference game page
+#
+# Outputs:
+#   A tuple of winner, loser, and number_of_fighting_penalties, where
+#   'fighting' encompasses both fighting and roughing penalties.
+###############################################################################
+def get_penalties_from_game(html):
+  soup = BeautifulSoup(html, "html.parser")
 
   scorebox = soup.find(class_="scorebox")
   performers = scorebox.find_all(itemprop="performer")
@@ -19,11 +29,31 @@ with open("example.html") as file:
     
     team_info[abbr] = score
   
-  winning_team = max(team_info, key=team_info.get)
+  winner = max(team_info, key=team_info.get)
+  loser = min(team_info, key=team_info.get)
 
   penalty_table = soup.find(id="penalty")
   fighting_penalties = penalty_table.find_all(string=re.compile("Fighting|Roughing"))
 
-  print(f"The {teams.team_name_by_abbr[winning_team]} won this game in which " +
-    f"there were {len(fighting_penalties)} fighting or roughing penalties.")
-file.close()
+  return (winner, loser, len(fighting_penalties))
+
+###############################################################################
+# get_game_urls_from_gamelog
+#
+# Inputs:
+#   html:     A string of raw html, scraped from a hockey-reference team
+#             season gamelog
+#   base_url: The url of the scraped website because for SOME REASON these
+#             hrefs are relative
+#
+# Outputs:
+#   A list of urls of hockey-reference game pages played by that team
+###############################################################################
+def get_game_urls_from_gamelog(html, base_url):
+  soup = BeautifulSoup(html, "html.parser")
+
+  gamelog = soup.find(id="tm_gamelog_rs")
+  game_rows = gamelog.find_all(id=re.compile("tm_gamelog_rs."))
+  link_containers = [game_row.find(attrs={"data-stat": "date_game"}) for game_row in game_rows]
+  links = [base_url + link_container.find("a").get("href") for link_container in link_containers]
+  return links
